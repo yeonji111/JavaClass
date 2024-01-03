e78ffba993548e2f59164f619df86469ba5ee2ab
yeonji <pyj10370160@gmail.com>
2024-01-03 13:34:12 +0900

    java.nextit_first commit

--- src/yeonji/submit06/Submit02.java
+++ src/yeonji/submit06/Submit02.java
@@ -13,15 +13,31 @@ for문을 이용해 intArr의 최댓값과 최솟값을 출력해주세요.
  */
 public class Submit02 {
     public static void main(String[] args) {
-        // 첫번째 방법) 메소드를 만든 후 오름차순 정렬해서 인덱스 0번과 인덱스 (배열 길이 - 1)번 출력하기
+        // 첫번째 방법) intArr
+        // 메소드를 만든 후 오름차순 정렬해서 인덱스 0번과 인덱스 (배열 길이 - 1)번 출력하기
         int[] intArr = {23, 456, 213, 32, 464, 1, 2, 4};
         // 오름차순 배열하고 인덱스 0번과 인덱스 (길이-1)을 출력
         Arrays.sort(intArr);
 //        printArray(intArr);
-        System.out.println("최솟값: " +intArr[0]); // 최솟값
-        System.out.println("최댓값: " +intArr[intArr.length-1]); // 최댓값
+        System.out.println("최솟값: " + intArr[0]); // 최솟값
+        System.out.println("최댓값: " + intArr[intArr.length - 1]); // 최댓값
 
 
+        System.out.println("\n============다른 방법=============\n");
+        // 두번째 방법) for,if문 넣어서 버블 정렬 ,
+        // int[] intArrB
+        int[] intArrB = {23, 456, 213, 32, 464, 1, 2, 4};
+        for (int j = 0; j < intArrB.length - 1 - j; j++) {
+            for (int i = 0; i < intArrB.length - 1; i++) {
+                if (intArrB[i] > intArrB[i + 1]) { // 크면 맨 한칸씩 뒤로 이동해, 한사이클
+                    int tmp = intArrB[i];
+                    intArrB[i] = intArrB[i + 1];
+                    intArrB[i + 1] = tmp;
+                }
+            }
+        }
+        System.out.println("최댓값: " + intArrB[(intArrB.length-1)]); //  최댓값
+        System.out.println("최솟값: " + intArrB[0]); //  최솟값
 
     }
 
--- src/yeonji/submit06/Submit03.java
+++ src/yeonji/submit06/Submit03.java
@@ -16,14 +16,21 @@ public class Submit03 {
         }
 
     } //(int) (Math.random() * 45) + 1) 1~45 로또 번호 랜덤 생성
-    public static int[] makeLotto(int n){
+    public static int[] makeLotto(int n) {
         int[] myLotto = new int[n]; // n개 짜리 배열(myLotto) 생성
         for (int i = 0; i < n; i++) { // myLotto 인덱스에 숫자 채우기
             myLotto[i] = (int) (Math.random() * 45) + 1;
-//            System.out.println(myLotto[i]);
+
+//            중복되지 않도록 인덱스 앞번호랑 뒷번호가 같다면 다시 반복
+            if (myLotto[i] == myLotto[n - 1 - i]) { // 인덱스 범위를 벗어났다고함
+                for(int j = 0; j < n; j++) {
+                    myLotto[i] = (int) (Math.random() * 45) + 1;
+                }
+
+
+            }
+
         }
-     return myLotto;
+        return myLotto;
     }
-
-
 }
